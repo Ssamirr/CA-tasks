909ebafaabe722dae313ec5dc172830e1f072c84
Samir <ssardarlisamir@gmail.com>
2022-11-30 14:52:03 +0400

    guess number

---
+++ guessNumber/main.js
@@ -0,0 +1,29 @@
+let guess_button = document.querySelector(".guess_button");
+let guess_input = document.querySelector(".guess_input");
+
+let randomNumber = Math.ceil(Math.random() * 15);
+console.log(randomNumber)
+
+
+let guessed_number = 0;
+
+guess_button.addEventListener("click", function () {
+
+    if (!(guess_input.value >= 1 && guess_input.value <= 15)) {
+        alert("Guess a number between 1-15");
+    } else {
+        guessed_number++;
+        if (randomNumber == guess_input.value) {
+            document.querySelector(".display").innerText = "Well done";
+            document.querySelector(".guessedNumber").innerText += randomNumber;
+            document.querySelector(".mainNumber").innerText += guessed_number;
+            guess_button.classList.add("pointerEvent")
+
+        } else if (randomNumber > guess_input.value) {
+            document.querySelector(".display").innerText = "Low-Try Again"
+        } else{
+            document.querySelector(".display").innerText = "High-Try Again"
+        } 
+    }
+
+})
